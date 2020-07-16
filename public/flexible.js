0c1d9c2276f6c05ea88b434dd23f7e1fe3e369d4
BokFang <1092554430@qq.com>
2020-07-17 00:19:22 +0800

    feat(app.vue):新增骨架屏效果 2020-7-16

--- public/flexible.js
+++ public/flexible.js
@@ -1,10 +1,19 @@
 const WIDTH = 1080; // 设计稿宽度
 const mobileAdapter = () => {
   let scale = screen.width / WIDTH; // 缩放比
-  let content ='width='+ WIDTH + ', initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale + ', user-scalable=0'
-  let meta = document.createElement('meta')
-  meta.setAttribute('name', 'viewport')
-  document.head.appendChild(meta)
-  meta.setAttribute('content', content)
-}
-mobileAdapter()
+  let content =
+    "width=" +
+    WIDTH +
+    ", initial-scale=" +
+    scale +
+    ", maximum-scale=" +
+    scale +
+    ", minimum-scale=" +
+    scale +
+    ", user-scalable=0";
+  let meta = document.createElement("meta");
+  meta.setAttribute("name", "viewport");
+  document.head.appendChild(meta);
+  meta.setAttribute("content", content);
+};
+mobileAdapter();
